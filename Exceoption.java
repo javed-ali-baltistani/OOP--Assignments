cf356b22a31fecb430eaf046410721dc633a4447
javed-ali-baltistani <96606642+javed-ali-baltistani@users.noreply.github.com>
2022-08-01 05:48:24 -0700

    Add files via upload

---
+++ Exceoption.java
@@ -0,0 +1,18 @@
+class Exption
+{
+public static void main (String args[])
+{
+int a=60;
+try
+{
+int c=a/args.length;
+System.out.println(c);
+args[5]="Ali";
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+System.out.println("after catch");
+}
+}
