6ea5be427d6bc0d7e294c068be6db02a1bfb7c02
javed-ali-baltistani <96606642+javed-ali-baltistani@users.noreply.github.com>
2022-08-01 05:54:25 -0700

    Add files via upload

---
+++ Demotwointerface.java
@@ -0,0 +1,39 @@
+interface Myinf1 extends Myinf{
+double compute(double n);
+}
+interface Myinf
+{
+double sum(double a,double b);
+
+void show();
+}
+class Myclass implements Myinf1
+{
+double ans;
+public double sum(double x,double y)
+{
+ans=x+y;
+return ans;
+}
+public double compute(double num)
+{
+return num*num;
+}
+
+public void show()
+{
+System.out.println("interface show method"+"  "+ans);
+}
+}
+class Demotwointerface
+{
+public static void main (String args [])
+{
+Myclass p=new Myclass();
+double ans1=p.sum(10,20);
+p.show();
+System.out.println("ans1 Answer"+"    "+ans1);
+System.out.println("compute method"+p.compute(6));
+
+}
+}
