83e928536127315641437443dd3abfea1141c98f
javed-ali-baltistani <96606642+javed-ali-baltistani@users.noreply.github.com>
2022-08-01 06:01:04 -0700

    Add files via upload

---
+++ Arraycmdarg.java
@@ -0,0 +1,12 @@
+public class ArraycmdArg
+{
+public static void main (String args[])
+{
+//int [] m=new int[args.length]
+ System.out.println("length of an array"+args.length);
+for (int i=0; i<args.length ;i++)
+{
+ System.out.println(args[i]);
+}
+}
+}
